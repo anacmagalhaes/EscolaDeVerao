8744041a72388a58c8ffd225ac5e12f2454b5f57
anamagalhaes <anacarolinasoaresmagalhaes@gmail.com>
2025-01-02 19:34:26 -0300

    feat: Crated App Bar Error Screens

---
+++ lib/widgets/custom_app_bar_error.dart
@@ -0,0 +1,47 @@
+import 'package:escoladeverao/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBarError extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBarError({
+    super.key,
+    required this.onBackPressed,
+    this.fallbackRoute,
+    this.backgroundColor = AppColors.background,
+    this.leadingIcon,
+  });
+
+  final VoidCallback onBackPressed;
+  final String? fallbackRoute;
+  final Color backgroundColor;
+  final Widget? leadingIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: backgroundColor,
+      leading:
+          leadingIcon != null // Verifica se leadingIcon é diferente de null
+              ? IconButton(
+                  icon: leadingIcon!,
+                  onPressed: () {
+                    FocusScope.of(context).unfocus();
+                    if (Navigator.canPop(context)) {
+                      Navigator.pop(context);
+                    } else if (fallbackRoute != null) {
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, fallbackRoute!, (route) => false);
+                    } else {
+                      onBackPressed();
+                    }
+                  },
+                )
+              : null, // Se for null, não exibe nada
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(color: Colors.transparent),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56);
+}
