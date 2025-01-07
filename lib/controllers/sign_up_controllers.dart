8c09f83cfc8164edb992e5081fc5c5a69244c90d
anamagalhaes <anacarolinasoaresmagalhaes@gmail.com>
2025-01-07 15:51:06 -0300

    feat: Added controllers Sign Up

---
+++ lib/controllers/sign_up_controllers.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+TextEditingController nameController = TextEditingController();
+TextEditingController emailController = TextEditingController();
+TextEditingController passwordController = TextEditingController();
+TextEditingController cpfController = TextEditingController();
+TextEditingController phoneController = TextEditingController();
+TextEditingController linkedinController = TextEditingController();
+TextEditingController githubController = TextEditingController();
+TextEditingController lattesController = TextEditingController();
+TextEditingController confirmPassController = TextEditingController();
