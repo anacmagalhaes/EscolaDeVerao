6afa5da8dffcfba51c180f2749d4e8e6d0f6068f
anamagalhaes <anacarolinasoaresmagalhaes@gmail.com>
2025-01-09 15:15:45 -0300

    talent: Added controllers for masks

--- lib/controllers/sign_up_controllers.dart
+++ lib/controllers/sign_up_controllers.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 
 TextEditingController nameController = TextEditingController();
 TextEditingController emailController = TextEditingController();
 TextEditingController passwordController = TextEditingController();
-TextEditingController cpfController = TextEditingController();
-TextEditingController phoneController = TextEditingController();
+TextEditingController cpfController =
+    MaskedTextController(mask: '000.000.000-00');
+TextEditingController phoneController =
+    MaskedTextController(mask: '(00) 00000-0000');
 TextEditingController linkedinController = TextEditingController();
 TextEditingController githubController = TextEditingController();
 TextEditingController lattesController = TextEditingController();
