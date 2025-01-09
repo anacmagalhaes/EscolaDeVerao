b5e2743adc6cef51e12e66d14b4fa99229a65e75
anamagalhaes <anacarolinasoaresmagalhaes@gmail.com>
2025-01-09 17:44:41 -0300

    feat: Added for passeord recovey

---
+++ lib/screens/modals/new_password_modal.dart
@@ -0,0 +1,125 @@
+import 'package:escoladeverao/screens/auth/login_screen.dart';
+import 'package:escoladeverao/utils/colors.dart';
+import 'package:escoladeverao/utils/fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void NewPasswordModal(BuildContext context, String email) {
+  String maskEmail(String email) {
+    // Divide o e-mail em duas partes: antes e depois do '@'
+    final parts = email.split('@');
+    if (parts.length != 2)
+      return email; // Retorna o e-mail original se estiver inválido
+
+    final username = parts[0];
+    final domain = parts[1];
+
+    // Mascara o nome de usuário (ex: ex**e)
+    final maskedUsername = username.length > 4
+        ? '${username.substring(0, 2)}**${username.substring(username.length - 1)}'
+        : username;
+
+    // Mascara o domínio (ex: ****.com)
+    final maskedDomain = domain.replaceRange(0, domain.indexOf('.'), '****');
+
+    return '$maskedUsername@$maskedDomain';
+  }
+
+  final maskedEmail = maskEmail(email);
+
+  showDialog(
+    context: context,
+    barrierColor:
+        AppColors.background.withOpacity(0.3), // Fundo semi-transparente
+    barrierDismissible: true, // Permite fechar o dialog tocando fora
+    builder: (context) {
+      return Dialog(
+        backgroundColor:
+            Colors.transparent, // Tornar o fundo do dialog transparente
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white, // Cor do conteúdo do dialog
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    child: Stack(
+                      alignment: Alignment
+                          .center, // Centraliza o círculo interno no externo
+                      children: [
+                        Container(
+                          width: 60.h,
+                          height: 60.h,
+                          decoration: BoxDecoration(
+                            color: AppColors.secondaryGreen,
+                            shape: BoxShape.circle, // Forma circular
+                          ),
+                        ),
+                        Container(
+                          width: 40.h, // Menor que o círculo externo
+                          height: 40.h,
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryGreen,
+                            shape: BoxShape.circle, // Forma circular
+                          ),
+                          child: GestureDetector(
+                            child: Image.asset('assets/icons/check-icon.png'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()));
+                    },
+                  ),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).pop(), // Fecha o modal
+                    child: Container(
+                      width: 60.h,
+                      height: 60.h,
+                      decoration: BoxDecoration(
+                        color: AppColors.white,
+                        shape: BoxShape.circle, // Forma circular
+                      ),
+                      child: Image.asset('assets/icons/close-icon.png'),
+                    ),
+                    onTapCancel: () {},
+                  ),
+                ],
+              ),
+              SizedBox(height: 12.h),
+              Column(
+                children: [
+                  Fonts(
+                    text: 'Nova senha enviada para o e-mail cadastrado',
+                    maxLines: 3,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.textPrimary,
+                  ),
+                  SizedBox(height: 4.h),
+                  Fonts(
+                    text: 'Verifique a caixa de entrada do e-mail $maskedEmail',
+                    maxLines: 4,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: AppColors.textPrimary.withOpacity(0.5),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
