608232fe13e5659b4f27605b9bf9e80ab1cc9634
anamagalhaes <anacarolinasoaresmagalhaes@gmail.com>
2025-01-13 15:21:04 -0300

    feat: Added Screen Change Password

---
+++ lib/controllers/change_password_controllers.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text2/flutter_masked_text2.dart';
+
+TextEditingController emailChangePassController = TextEditingController();
+TextEditingController cpfChangePassController =
+    MaskedTextController(mask: '000.000.000-00');
