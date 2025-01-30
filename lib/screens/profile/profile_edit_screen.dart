e78a629760c57145026ae9706512bfcb967178d4
anamagalhaes <anacarolinasoaresmagalhaes@gmail.com>
2025-01-30 13:12:33 -0300

    feat: Added for edit profile

---
+++ lib/screens/profile/profile_edit_screen.dart
@@ -0,0 +1,259 @@
+import 'package:escoladeverao/controllers/profile_edit_controller.dart';
+import 'package:escoladeverao/models/user_model.dart';
+import 'package:escoladeverao/screens/profile/profile_screen.dart';
+import 'package:escoladeverao/screens/settings_screen.dart';
+import 'package:escoladeverao/utils/colors.dart';
+import 'package:escoladeverao/utils/fonts.dart';
+import 'package:escoladeverao/widgets/custom_outlined_button.dart';
+import 'package:escoladeverao/widgets/custom_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ProfileEditScreen extends StatefulWidget {
+  final String origin;
+  final User user;
+  final User scannedUser;
+  const ProfileEditScreen(
+      {Key? key,
+      required this.origin,
+      required this.scannedUser,
+      required this.user})
+      : super(key: key);
+
+  @override
+  _ProfileEditScreenState createState() => _ProfileEditScreenState();
+}
+
+class _ProfileEditScreenState extends State<ProfileEditScreen> {
+  late ScrollController _scrollController;
+  bool _showAppBar = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    if (_scrollController.offset > 50 && _showAppBar) {
+      setState(() {
+        _showAppBar = false;
+      });
+    } else if (_scrollController.offset <= 50 && !_showAppBar) {
+      setState(() {
+        _showAppBar = true;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(147.h),
+        child: Container(
+          width: double.infinity,
+          color: AppColors.orangePrimary,
+          child: Padding(
+            padding: EdgeInsets.only(left: 10.h, right: 10.h, top: 35.h),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if (widget.origin == 'settings') {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SettingsScreen(
+                                  user: widget.user,
+                                  scannedUser: widget.scannedUser,
+                                )),
+                      );
+                    } else if (widget.origin == 'profile') {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfileScreen(
+                                  user: widget.user,
+                                )),
+                      );
+                    } else {
+                      Navigator.pop(context); // Fallback para outras telas
+                    }
+                  },
+                  child: SizedBox(
+                    width: 44.h,
+                    height: 44.h,
+                    child: Image.asset('assets/icons/angle-left-white.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Stack(
+          children: [
+            // Orange background container
+            Container(
+              height: 200.h,
+              color: AppColors.orangePrimary,
+            ),
+
+            // White background container with rounded corners
+            Container(
+              margin: EdgeInsets.only(top: 120.h),
+              decoration: const BoxDecoration(
+                color: AppColors.background,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.h),
+                child: Column(
+                  children: [
+                    SizedBox(height: 80.h), // Space for profile image
+                    const Fonts(
+                      text: 'Allyson Steve Mota Lacerda',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      color: AppColors.textPrimary,
+                    ),
+                    SizedBox(height: 8.h),
+                    const Fonts(
+                      text: 'ID: ',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700,
+                      color: AppColors.blueMarine,
+                      additionalSpans: [
+                        TextSpan(
+                          text: '0000',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.blueMarine,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10.h),
+                    CustomTextField(
+                      labelText: 'Nome',
+                      hintText: 'Edite seu nome',
+                      keyboardType: TextInputType.name,
+                      controller: nameEditController,
+                    ),
+                    SizedBox(height: 20.h),
+                    CustomTextField(
+                      labelText: 'E-mail',
+                      hintText: 'Edite seu e-mail',
+                      keyboardType: TextInputType.emailAddress,
+                      controller: emailEditController,
+                    ),
+                    SizedBox(height: 20.h),
+                    CustomTextField(
+                      labelText: 'Telefone',
+                      hintText: 'Edite seu telefone',
+                      keyboardType: TextInputType.phone,
+                      controller: phoneEditController,
+                    ),
+                    SizedBox(height: 20.h),
+                    CustomTextField(
+                      labelText: 'LinkedIn',
+                      hintText: 'Edite seu LinkedIn',
+                      keyboardType: TextInputType.url,
+                      controller: linkedinEditController,
+                    ),
+                    SizedBox(height: 20.h),
+                    CustomTextField(
+                      labelText: 'GitHub',
+                      hintText: 'Edite seu GitHub',
+                      keyboardType: TextInputType.url,
+                      controller: githubEditController,
+                    ),
+                    SizedBox(height: 20.h),
+                    CustomTextField(
+                      labelText: 'Currículo Lattes',
+                      hintText: 'Edite seu Lattes',
+                      keyboardType: TextInputType.url,
+                      controller: latesEditController,
+                    ),
+                    SizedBox(height: 22.h),
+                    CustomOutlinedButton(
+                      text: 'Salvar alterações',
+                      height: 56.h,
+                      width: double.maxFinite,
+                      buttonFonts: const Fonts(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.background,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      buttonStyle: OutlinedButton.styleFrom(
+                        side: const BorderSide(color: AppColors.orangePrimary),
+                        backgroundColor: AppColors.orangePrimary,
+                      ),
+                      onPressed: () {
+                        // Implementar lógica de salvar alterações
+                      },
+                    ),
+                    SizedBox(height: 20.h), // Add some bottom padding
+                  ],
+                ),
+              ),
+            ),
+
+            // Positioned profile image
+            Positioned(
+              top: 120.h - 52.h,
+              left: MediaQuery.of(context).size.width / 2 - 52.h,
+              child: SizedBox(
+                width: 104.h,
+                height: 104.h,
+                child: ClipOval(
+                  child: Image.asset(
+                    'assets/images/person.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+
+            // Edit profile image icon
+            Positioned(
+              top: 120.h + 24.h,
+              left: MediaQuery.of(context).size.width / 2 + 24.h,
+              child: GestureDetector(
+                onTap: () {
+                  // Ação para editar a imagem
+                },
+                child: Container(
+                  width: 27.h,
+                  height: 27.h,
+                  decoration: const BoxDecoration(
+                    color: AppColors.orangePrimary,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Image.asset('assets/icons/pen_black_icon.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
