5bdf9170faf07aa03d87adb66362a010bcf5fe55
anamagalhaes <anacarolinasoaresmagalhaes@gmail.com>
2025-01-31 15:59:06 -0300

    feat: renamed files

---
+++ lib/utils/colors_utils.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+//classe para cores personalizadas
+class AppColors {
+  static const Color orangePrimary = Color(0xFFFF9E1B);
+  static const Color orangeSecond = Color(0xFFE7731F);
+  static const Color background = Color(0xFFFFF8F0);
+  static const Color textPrimary = Color(0xFF232323);
+  static const Color grey = Color(0xFFE8E8E8);
+  static const Color red = Color(0xFFFF0303);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color blueMarine = Color(0xFF0D1B34);
+  static const Color black = Color(0xFF000000);
+  static const Color secondaryGrey = Color(0xFFADADAD);
+  static const Color terciaryGrey = Color(0xFFCACACA);
+  static const Color quaternaryGrey = Color(0xFF969696);
+  static const Color primaryGreen = Color(0xFFC7F9D9);
+  static const Color secondaryGreen = Color(0xFFE3FCED);
+  static const Color quintennialGrey = Color(0xFF7D7D7D);
+  static const Color blue = Color(0xFF3E94E9);
+}
