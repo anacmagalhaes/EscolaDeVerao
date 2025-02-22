4d20c3f13c01011312e916ad199755e41b4d6520
anamagalhaes <anacarolinasoaresmagalhaes@gmail.com>
2025-02-22 18:11:36 -0300

    feat: commented unused

--- lib/controllers/password_controller.dart
+++ lib/controllers/password_controller.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_masked_text2/flutter_masked_text2.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 
-TextEditingController emailPassController = TextEditingController();
-TextEditingController cpfPassController =
-    MaskedTextController(mask: '000.000.000-00');
+// TextEditingController emailPassController = TextEditingController();
+// TextEditingController cpfPassController =
+//     MaskedTextController(mask: '000.000.000-00');
